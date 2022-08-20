321a140a91153df7343eff65ac11305f5938a4ee
zhangn <zhangn@123>
2022-08-20 18:55:47 +0800

    更新了readme

--- src/components/HeaderPage/HeaderPage.vue
+++ src/components/HeaderPage/HeaderPage.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
 import Avatar from './Avatar.vue'
-import {Money16Regular,Accessibility16Filled} from '@vicons/fluent'
-
-import { Icon } from '@vicons/utils'
 </script>
 
 <template>
-    <div>我是子组件</div>
+    <div>极简协作</div>
     <Avatar></Avatar>
-    <Icon color="green" size="80">
-        <Money16Regular />
-        <Accessibility16Filled/>
-    </Icon>
+
 </template>
 
 <style scoped>
