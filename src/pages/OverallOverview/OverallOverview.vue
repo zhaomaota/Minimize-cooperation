c93e355abb4415fd89c1d185e3811fc294810a40
zn123456 <ningzhang_d>
2022-09-08 21:02:33 +0800

    新增

--- src/pages/OverallOverview/OverallOverview.vue
+++ src/pages/OverallOverview/OverallOverview.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-    
+// 声明一个myInterface接口，规定了使用该接口的类里面有且仅有name和age两个属性，其数据类型需符合接口定义
+
+class Person{
+    private name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+}
+
+const per = new Person('孙悟空',18)
 </script>
 
 <template>
-    <div>我是全局总览</div>
+  <div>我是全局总览</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
